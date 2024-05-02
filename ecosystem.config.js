c50fee535d8e7ea1f6cc5896f234b85c3811fcb2
Siddharth Iyer <siye0010@student.monash.edu>
2024-05-02 23:20:47 +1000

    removed watch

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,32 +1,24 @@
 module.exports = {
-    apps : [{
-      name: "padstation-backend",
-      script: "run.py",
-      interpreter: "python3",
-      env: {
-        NODE_ENV: "production",
-      },
-      watch: true,
-      ignore_watch : ["node_modules", "logs", ".git"],
-      watch_options: {
-        "followSymlinks": false
-      },
-      restart_delay: 3000
+    apps: [{
+        name: "padstation-backend",
+        script: "run.py",
+        interpreter: "python3",
+        env: {
+            NODE_ENV: "production",
+        },
+        watch: false,
+        restart_delay: 3000
     },
     {
-      name: "padstation-frontend",
-      script: "npm",
-      args: "run prod",
-      interpreter: "none",
-      cwd: "./frontend",
-      env: {
-        NODE_ENV: "production",
-      },
-      watch: true,
-      ignore_watch : ["node_modules", "logs", ".git"],
-      watch_options: {
-        "followSymlinks": false
-      },
-      restart_delay: 3000
+        name: "padstation-frontend",
+        script: "npm",
+        args: "run prod",
+        interpreter: "none",
+        cwd: "./frontend",
+        env: {
+            NODE_ENV: "production",
+        },
+        watch: false,
+        restart_delay: 3000
     }]
-  }
+}
