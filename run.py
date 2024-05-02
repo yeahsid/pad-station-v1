ba82ab8f050851343fc609b21d188174954c3597
Siddharth Iyer <siye0010@student.monash.edu>
2024-05-02 22:58:09 +1000

    feat: added gunicorn

--- run.py
+++ run.py
@@ -1,14 +1,30 @@
 from app.main import app
-import uvicorn
+from uvicorn.workers import UvicornWorker
+from gunicorn.app.base import BaseApplication
+
+class Application(BaseApplication):
+
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        config = {key: value for key, value in self.options.items()
+                  if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
 
 if __name__ == "__main__":
     """
-    This script runs the application using uvicorn server.
+    This script runs the application using uvicorn server with gunicorn.
     It starts the server on the specified host and port.
     """
-    server = uvicorn.Server(config=uvicorn.Config(app, host="0.0.0.0", port=8000))
-    try:
-        server.run()
-    except KeyboardInterrupt:
-        server.shutdown()
-        print("\nServer stopped.")
+    options = {
+        'bind': '%s:%s' % ('0.0.0.0', '8000'),
+        'workers': 4,
+    }
+    Application(app, options).run()
