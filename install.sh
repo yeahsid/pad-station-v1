46ce46d965f0ae68cd486c608b4d3c320dfefcd1
Siddharth Iyer <siye0010@student.monash.edu>
2024-07-15 07:18:14 +1000

    fix: fixed install script for nvm

--- install.sh
+++ install.sh
@@ -49,7 +49,7 @@ pipx ensurepath
 
 # Install nodejs with NVM.
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | zsh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
