8bb53f877935d5c788b8268589838840b20d8e8a
RachitTamrakar <Rtam0011@student.monash.edu>
2024-10-29 16:11:46 -0700

    Add initial project structure

---
+++ backend/backend/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, FastAPI with Poetry!"}
---
+++ frontend/src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+	export let name;
+
+	let message = "";
+
+	async function fetchMessage() {
+		const response = await fetch("http://localhost:8000/");
+		const data = await response.json();
+		message = data.message;
+	}
+
+  fetchMessage();
+</script>
+
+
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>{message}</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
